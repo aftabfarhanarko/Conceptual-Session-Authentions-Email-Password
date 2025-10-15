be4329a6b0423a18f1a8279d4cfabedac7560f5e
Aftab Farhan <aftabfarhan324@gmail.com>
2025-10-16 01:58:27 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from '../../assets/react.svg'
+import { NavLink } from "react-router";
+import './nav.css'
+
+const Navbar = () => {
+  return (
+   <div className="bg-white/80">
+    <div className="flex items-center justify-between max-w-[1300px] mx-auto  p-3 text-black">
+   <div className="border-2 p-2 rounded-full bg-base-300  ">
+     <img src={logo}></img>
+   </div>
+     <div>
+        <NavLink className="mr-4" to="/">Home</NavLink>
+        <NavLink className="mr-4" to="/profile">Profile</NavLink>
+        <NavLink className="mr-4" to="/about">AboutUs</NavLink>
+        <NavLink className="mr-4" to="/signin">SignIn</NavLink>
+        <NavLink className="mr-4" to="/signup">SignUp</NavLink>
+     </div>
+     <div>
+       <button className="btn btn-secondary">Sign In</button>
+
+     </div>
+   </div>
+   </div>
+  );
+};
+
+export default Navbar;
